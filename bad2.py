7e76f2527185393034382ec2b3fa93e3dbd34d2d
duriantaco <aaronoh2015@gmail.com>
2026-01-16 15:19:37 +0800

    test

--- bad2.py
+++ bad2.py
@@ -9,6 +9,9 @@ eval(input())
 if False:
     getattr(sys.modules[__name__], "getattr_trick_unused")
 
+if False:
+    getattr(sys.modules[__name__], "getattr_trick_unused2")
+
 
 ## unused
 def string_annotation_unused() -> "string_annotation_unused":
---
+++ vuln.py
@@ -0,0 +1,11 @@
+import os
+os.system(input())
+
+unused_code = 'asdf'
+used_code = 'abc'
+print(used_code)
+
+## unused
+if False:
+    getattr(sys.modules[__name__], "getattr_trick_unused")
+
