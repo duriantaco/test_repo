b2d68a0ce74288f357144c240c0d9770a0e7a987
duriantaco <aaronoh2015@gmail.com>
2026-01-17 13:22:46 +0800

    test changes

---
+++ bad3.py
@@ -0,0 +1,23 @@
+import matplotlib
+import time
+import os
+import os
+
+eval(input())
+
+## unused
+if False:
+    getattr(sys.modules[__name__], "getattr_trick_unused")
+
+if False:
+    getattr(sys.modules[__name__], "getattr_trick_unused2")
+
+
+if False:
+    getattr(sys.modules[__name__], "getattr_trick_unused3")
+
+
+## unused
+def string_annotation_unused() -> "string_annotation_unused":
+    return "I am only used in my own type hint string"
+
